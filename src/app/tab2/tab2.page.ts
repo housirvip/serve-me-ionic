394ead12a23400a17c2fccdaebc2fb811f24c630
housirvip <housirvip@outlook.com>
2020-02-02 16:15:24 -0600

    update some page

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {AlertController} from '@ionic/angular';
 
 @Component({
-  selector: 'app-tab2',
-  templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+    selector: 'app-tab2',
+    templateUrl: 'tab2.page.html',
+    styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+    items: any[];
 
-  constructor() {}
+    constructor(private alertController: AlertController) {
+        this.items = [{key: 'qwe0', value: 'qweqweqweqwe'}];
+        for (let i = 1; i < 6; i++) {
+            this.items.push({key: 'qwe' + i, value: 'asdasdasdasd'});
+        }
+    }
 
+    doRefresh(event) {
+        setTimeout(() => {
+            this.items.push({key: '123', value: '123123123'});
+            event.target.complete();
+        }, 1000);
+    }
+
+    async read(item: any) {
+        const alert = await this.alertController.create({
+            header: item.key,
+            // subHeader: 'Subtitle',
+            message: item.value,
+            buttons: ['OK']
+        });
+
+        await alert.present();
+    }
 }
