0c85a0e2ff38854507bff311c14eeda9354c1c99
housirvip <housirvip@outlook.com>
2020-02-02 18:48:26 -0600

    update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://serve-me-backend.herokuapp.com/'
+    apiUrl: 'https://serve-me-java.herokuapp.com/'
     // apiUrl: 'http://192.168.1.139/'
 };
