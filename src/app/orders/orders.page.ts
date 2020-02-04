10475b79483f5541177b46f6f4d777e194187602
housirvip <housirvip@outlook.com>
2020-02-03 23:08:41 -0600

    update orders.page.ts fix 1 bug

--- src/app/orders/orders.page.ts
+++ src/app/orders/orders.page.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {OrderService} from '../services/order.service';
 import {Order} from '../classes/orders/order';
 
 @Component({
-  selector: 'app-orders',
-  templateUrl: './orders.page.html',
-  styleUrls: ['./orders.page.scss'],
+    selector: 'app-orders',
+    templateUrl: './orders.page.html',
+    styleUrls: ['./orders.page.scss'],
 })
 export class OrdersPage implements OnInit {
 
@@ -14,13 +14,12 @@ export class OrdersPage implements OnInit {
     constructor(private orderService: OrderService) {
     }
 
-  ngOnInit() {
-   this.orderService.RequestOrder();
-   this.orders = this.orderService.order;
-  }
+    ngOnInit() {
+        this.orders = this.orderService.getOrder();
+    }
 
-  segmentChanged(ev: any) {
-    console.log('Segment changed', ev);
-  }
+    segmentChanged(ev: any) {
+        console.log('Segment changed', ev);
+    }
 
 }
