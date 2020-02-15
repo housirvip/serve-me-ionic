9bc5fd9fb08b8586f73c8849626a6871571a8ce6
housirvip <housirvip@outlook.com>
2020-02-14 19:23:15 -0600

    remove tabs, use fab instead

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -2,8 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {UserService} from '../services/user.service';
 import {FirebaseService} from '../services/firebase.service';
 
-import {ModalController} from '@ionic/angular';
-import {Router, RouterEvent} from '@angular/router';
+import {MenuController, ModalController} from '@ionic/angular';
+import {Router} from '@angular/router';
 import {LoginPage} from '../login/login.page';
 import {RegisterPage} from '../register/register.page';
 
@@ -16,23 +16,33 @@ export class TabsPage implements OnInit {
 
     menuPages = [
         {
+            title: 'dashboard',
+            url: '/tabs/dashboard',
+            icon: 'home-outline'
+        },
+        {
             title: 'profile',
             url: '/tabs/profile',
-            icon: 'person'
+            icon: 'person-outline'
+        },
+        {
+            title: 'orders',
+            url: '/tabs/orders',
+            icon: 'clipboard-outline'
         },
         {
             title: 'settings',
             url: '/tabs/settings',
-            icon: 'settings'
+            icon: 'settings-outline'
         },
         {
             title: 'service provider',
             url: '/tabs/settings',
-            icon: 'hammer'
+            icon: 'hammer-outline'
         }
     ];
 
-    selectedMenuPath = '';
+    // selectedMenuPath = '';
 
     get user() {
         return this.userService.user;
@@ -41,14 +51,17 @@ export class TabsPage implements OnInit {
     constructor(private userService: UserService,
                 private firebaseService: FirebaseService,
                 private router: Router,
+                private menu: MenuController,
                 private modalController: ModalController) {
-        this.router.events.subscribe((event: RouterEvent) => {
-            console.log('router event trigged:' + event.url);
-            this.selectedMenuPath = event.url;
-        });
+        // this.router.events.subscribe((event: RouterEvent) => {
+        //     console.log('router event trigged:' + event.url);
+        //     this.selectedMenuPath = event.url;
+        // });
     }
 
     ngOnInit(): void {
+        this.menu.enable(true, 'first').then(r => {
+        });
         this.userService.getUser();
         this.firebaseService.notifyToUpdate();
     }
@@ -71,5 +84,10 @@ export class TabsPage implements OnInit {
 
     toLogout() {
         this.userService.logout();
+    }
+
+    openMenu() {
+        this.menu.open('first').then(r => {
+        });
     }
 }
