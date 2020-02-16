0a43e7bac3bce0d73ba4d1be3113616904e91023
jiamingpan <821269398@QQ.COM>
2020-02-16 13:58:35 +0800

    Merge branch 'master' of /Users/jiamingpan/WebstormProjects/serve-me-ionic with conflicts.

---
+++ src/app/tabs/tabs.page.ts
@@ -0,0 +1,75 @@
+import {Component, OnInit} from '@angular/core';
+import {UserService} from '../services/user.service';
+import {FirebaseService} from '../services/firebase.service';
+
+import {ModalController} from '@ionic/angular';
+import {Router, RouterEvent} from '@angular/router';
+import {LoginPage} from '../login/login.page';
+import {RegisterPage} from '../register/register.page';
+
+@Component({
+    selector: 'app-tabs',
+    templateUrl: 'tabs.page.html',
+    styleUrls: ['tabs.page.scss']
+})
+export class TabsPage implements OnInit {
+
+    menuPages = [
+        {
+            title: 'service provider',
+            url: '/tabs/settings',
+            icon: 'hammer'
+        },
+        {
+            title: 'profile',
+            url: '/tabs/profile',
+            icon: 'person'
+        },
+        {
+            title: 'settings',
+            url: '/tabs/settings',
+            icon: 'settings'
+        }
+    ];
+
+    selectedMenuPath = '';
+
+    get user() {
+        return this.userService.user;
+    }
+
+    constructor(private userService: UserService,
+                private firebaseService: FirebaseService,
+                private router: Router,
+                private modalController: ModalController) {
+        this.router.events.subscribe((event: RouterEvent) => {
+            console.log('router event trigged:' + event.url);
+            this.selectedMenuPath = event.url;
+        });
+    }
+
+    ngOnInit(): void {
+        this.userService.getUser();
+        this.firebaseService.notifyToUpdate();
+    }
+
+    async toLogin() {
+        const modal = await this.modalController.create({
+            component: LoginPage,
+            cssClass: 'medium-modal'
+        });
+        return await modal.present();
+    }
+
+    async toRegister() {
+        const modal = await this.modalController.create({
+            component: RegisterPage,
+            cssClass: 'medium-modal'
+        });
+        return await modal.present();
+    }
+
+    toLogout() {
+        this.userService.logout();
+    }
+}
