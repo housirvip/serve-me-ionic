fa5c56c944a1e93dec8788999d4c233e2fce46a1
jiamingpan <821269398@QQ.COM>
2020-02-19 15:32:57 +0800

    fixBug + update email

---
+++ src/app/profile/update-email/update-email.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-update-email',
+  templateUrl: './update-email.page.html',
+  styleUrls: ['./update-email.page.scss'],
+})
+export class UpdateEmailPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
