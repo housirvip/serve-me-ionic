8e46ea2732a6fdf89a99871f66abf4dd71e16459
Javier Castro <javieralexcastro95@gmail.com>
2020-04-13 18:47:07 -0500

    added vendor seeing order review in completed tab

--- src/app/vendor-order/completed/completed.component.ts
+++ src/app/vendor-order/completed/completed.component.ts
@@ -1,49 +1,94 @@
-import {Component, OnInit} from '@angular/core';
-import {BidRequest} from '../../classes/spec/bid-request';
-import {OrderService} from '../../services/order.service';
-import {UserService} from '../../services/user.service';
-import {ToastService} from '../../services/toast.service';
-import {Vendor} from '../../classes/vendor';
-import {Bid} from '../../classes/bid';
-import {forEach} from '@angular-devkit/schematics';
+import { Component, OnInit } from "@angular/core";
+import { BidRequest } from "../../classes/spec/bid-request";
+import { OrderService } from "../../services/order.service";
+import { UserService } from "../../services/user.service";
+import { ToastService } from "../../services/toast.service";
+import { Vendor } from "../../classes/vendor";
+import { Bid } from "../../classes/bid";
+import { Order } from "../../classes/order";
+import { forEach } from "@angular-devkit/schematics";
+import { OrderStatus } from "../../classes/order-status";
+import { OrderRequest } from "../../classes/spec/order-request";
+import { AlertController } from "@ionic/angular";
 
 @Component({
-    selector: 'app-completed',
-    templateUrl: './completed.component.html',
-    styleUrls: ['./completed.component.scss'],
+  selector: "app-completed",
+  templateUrl: "./completed.component.html",
+  styleUrls: ["./completed.component.scss"],
 })
 export class CompletedComponent implements OnInit {
-    bidRequest: BidRequest;
-    haveTargetOrder = false;
+  bidRequest: BidRequest;
+  haveTargetOrder = false;
 
-    constructor(private orderService: OrderService,
-                private userService: UserService,
-                private toastService: ToastService) {
-        this.bidRequest = new BidRequest();
-        // plz review BidRequest, there shows some specification
-    }
+  filterRequest: OrderRequest;
 
-    get vendor(): Vendor {
-        return this.userService.vendor;
-    }
+  constructor(
+    private orderService: OrderService,
+    private userService: UserService,
+    private toastService: ToastService,
+    private alertController: AlertController
+  ) {
+    this.bidRequest = new BidRequest();
+    // plz review BidRequest, there shows some specification
+    this.filterRequest = new OrderRequest();
+    this.filterRequest.vendor = this.userService.vendor.id;
+  }
 
-    get bids(): Bid[] {
-        return this.orderService.bids;
-    }
+  get vendor(): Vendor {
+    return this.userService.vendor;
+  }
 
-    get orders() {
-        return this.orderService.orders;
-    }
+  get bids(): Bid[] {
+    return this.orderService.bids;
+  }
 
-    ngOnInit() {
-        console.log('pending component init');
-        this.bidRequest.vendor = this.vendor.id;
-        this.getBids();
+  get orders() {
+    return this.orderService.orders;
+  }
 
-    }
+  ngOnInit() {
+    console.log("pending component init");
+    this.bidRequest.vendor = this.vendor.id;
+    this.getBids();
+    this.getOrders(OrderStatus.Completed);
+    console.log(this.bids);
+    console.log(this.orders);
+  }
 
-    getBids() {
-        this.orderService.getBids(this.bidRequest);
-    }
+  getBids() {
+    this.orderService.getBids(this.bidRequest);
+  }
 
+  getOrders(status: OrderStatus) {
+    this.filterRequest.statusIn = [];
+    this.filterRequest.statusIn.push(status);
+    this.orderService.getOrders(this.filterRequest);
+  }
+
+  openReviewDetails(order: Order) {
+    console.log(order);
+    this.presentAlert(order);
+  }
+
+  async presentAlert(order: Order) {
+    const alert = await this.alertController.create({
+      header: "Review",
+      message:
+        "<b>Title:</b> " +
+        order.review.title +
+        "<br> <br>" +
+        "<b>Description:</b> " +
+        order.review.description,
+      buttons: [
+        {
+          text: "Ok",
+          handler: () => {
+            console.log("Ok button clicked");
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+  }
 }
