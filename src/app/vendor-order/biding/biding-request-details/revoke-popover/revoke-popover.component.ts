449b6bf1529d4821cb3bdf42a776dd82d532bfbc
hensby <whc0721a@outlook.com>
2020-04-29 10:36:35 -0700

    format

--- src/app/vendor-order/biding/biding-request-details/revoke-popover/revoke-popover.component.ts
+++ src/app/vendor-order/biding/biding-request-details/revoke-popover/revoke-popover.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from "@angular/core";
-import { NavParams, PopoverController } from "@ionic/angular";
-import { ToastService } from "../../../../services/toast.service";
-import { OrderService } from "../../../../services/order.service";
-import { Order } from "../../../../classes/order";
-import { Bid } from "../../../../classes/bid";
-import { UserService } from "../../../../services/user.service";
+import { Component, OnInit } from '@angular/core';
+import { NavParams, PopoverController } from '@ionic/angular';
+import { ToastService } from '../../../../services/toast.service';
+import { OrderService } from '../../../../services/order.service';
+import { Order } from '../../../../classes/order';
+import { Bid } from '../../../../classes/bid';
+import { UserService } from '../../../../services/user.service';
 
 @Component({
-  selector: "app-revoke-popover",
-  templateUrl: "./revoke-popover.component.html",
-  styleUrls: ["./revoke-popover.component.scss"],
+  selector: 'app-revoke-popover',
+  templateUrl: './revoke-popover.component.html',
+  styleUrls: ['./revoke-popover.component.scss'],
 })
 export class RevokePopoverComponent implements OnInit {
   order: Order;
@@ -23,22 +23,22 @@ export class RevokePopoverComponent implements OnInit {
     private popoverController: PopoverController,
     private navParams: NavParams
   ) {
-    this.order = this.navParams.get("order");
-    this.bid = this.navParams.get("bid");
+    this.order = this.navParams.get('order');
+    this.bid = this.navParams.get('bid');
   }
 
   ngOnInit() {
     this.revoked = false;
-    console.log("Order in popover: ", this.order);
-    console.log("Bid in popover", this.bid);
+    console.log('Order in popover: ', this.order);
+    console.log('Bid in popover', this.bid);
   }
 
   onRevokePressed() {
     this.revoked = true;
-    //call api function that removes bid
+    // call api function that removes bid
     this.orderService.deleteBid(this.bid.id);
     this.dissmissPopover().then((r) => {});
-    this.toastService.presentToast("Bid was revoked!", 1000).then((r) => {});
+    this.toastService.presentToast('Bid was revoked!', 1000).then((r) => {});
   }
 
   onCancelPressed() {
